123e3cea56aba49bfbbfcf1c7a0ed143ba31fa43
Kashish Goyal <Kashishgoyal961@gmail.com>
2022-11-21 23:56:35 +0530

    setting up passport

---
+++ config/passport-local-strategy.js
@@ -0,0 +1,71 @@
+// file for authenticate user who is trying to sign in.
+
+// requiring passport
+const passport = require('passport');
+
+// requiring local strategy for local level authentication
+const LocalStrategy = require('passport-local').Strategy;
+
+// requiring user schema->i havent made one yet
+// const User = require('../models/user_schema');
+
+// telling passport to use local strategy for local level authentication
+passport.use(new LocalStrategy({
+    // setting the unique feild by which we want to authenticate or find user
+    usernameField: 'email'
+},
+    function (email, password, done) {
+        User.findOne({ email: email }, function (err, user) {
+            if (err) {
+                console.log("error in finding the user-->passport");
+                return done(err);
+            }
+
+            if (!user || user.password != password) {
+                console.log("invalid username/password");
+                return done(null, false);
+            }
+            // sending authenticated user
+            return done(null, user);
+        });
+    }
+));
+
+// now we need to tell passport to which key to put in the coolies either username ,id or other keys
+// it will only tell not put cokkie there.
+passport.serializeUser(function (user, done) {
+    // sending user id
+    return done(null, user.id);
+});
+
+// now when the cookie come with the req from the server it is encrypter and to authenticate we need to derypt it
+passport.deserializeUser(function (id, done) {
+    User.findById(id, function (err, user) {
+        if (err) {
+            console.log("error in finding the user-->passport");
+            return done(err);
+        }
+        // sending user
+        return done(null, user);
+    })
+
+});
+
+
+passport.checkAuthentication = function (req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+
+    return res.redirect('/users/sign-in');
+}
+
+passport.setAuthenticatedUser = function (req, res, next) {
+    if (req.isAuthenticated()) {
+        res.locals.user = req.user;
+    }
+    return next();
+}
+
+// exporting passport not strategy
+module.exports = passport;
--- index.js
+++ index.js
@@ -2,9 +2,33 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+// for setting up cokkies
+// for putting encrypted cookies into browser
+const session = require('express-session');
+const passport = require('passport');
+const passportLocal = require('./config/passport-local-strategy');
+
 // setting up view engine
 app.set('view engine', 'ejs');
-app.set('views' , './views');
+app.set('views', './views');
+
+// encrypting the cookie
+app.use(session({
+    name: 'codeail',
+    secret: 'blahblahblah',
+    resave: false,
+    saveUninitialized: false,
+    // setting cookie age
+    cokkie: {
+        maxAge: (1000 * 60 * 100)
+    }
+}));
+
+// using passport || authenticating
+app.use(passport.initialize());
+// setting coolie into the browser
+app.use(passport.session());
+
 
 // requiring the connection between mongoose and mongodb
 const db = require('./config/mongoose');
